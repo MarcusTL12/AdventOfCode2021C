ea11192debef16630eb78915a95a02f5e5edc53a
MarcusTL12 <marcuspaafjellet@gmail.com>
2021-12-03 16:02:56 +0100

    Not working

--- src/day3.c
+++ src/day3.c
@@ -37,8 +37,8 @@ void d3p1() {
     uint64_t g = 0;
 
     for (size_t bit = 0; bit < bits; bit++) {
-        bool val = 2 * bitfield_count(inp + bit * bitints, bitints) > lines;
-        g = (g << 1) | val;
+        g = (g << 1) |
+            (2 * bitfield_count(inp + bit * bitints, bitints) > lines);
     }
 
     uint64_t mask = ~0;
@@ -51,4 +51,55 @@ void d3p1() {
     free(inp);
 }
 
-void d3p2() {}
+uint64_t single_out(uint64_t *inp, uint64_t *filtered, uint64_t *buffer,
+                    size_t bits, size_t lines, size_t bitints, bool inverted) {
+    memset(filtered, 0, bitints * sizeof(uint64_t));
+    bitfield_not(filtered, lines);
+
+    for (size_t bit = 0; bit < bits && bitfield_count(filtered, bitints) > 1;
+         bit++) {
+        memcpy(buffer, inp + bit * bitints, bitints * sizeof(uint64_t));
+
+        bitfield_and(buffer, filtered, bitints);
+        size_t ones = bitfield_count(buffer, bitints),
+               total = bitfield_count(filtered, bitints);
+
+        bool bitval = 2 * ones >= total;
+
+        if (inverted ^ !bitval) {
+            bitfield_not(buffer, lines);
+        }
+
+        bitfield_and(filtered, buffer, bitints);
+    }
+
+    size_t index = bitfield_findfirst_true(filtered, bitints);
+
+    uint64_t n = 0;
+
+    for (size_t bit = 0; bit < bits; bit++) {
+        n = (n << 1) | bitfield_get(inp + bit * bitints, index);
+    }
+
+    return n;
+}
+
+void d3p2() {
+    printf("THIS SOLUTION DOES NOT WORK!\n");
+
+    size_t bits, lines, bitints;
+    uint64_t *inp = load_inputs("input/day3/input", &bits, &lines, &bitints);
+    uint64_t *filtered = bitfield_create(lines),
+             *buffer = bitfield_create(lines);
+
+    uint64_t ox =
+        single_out(inp, filtered, buffer, bits, lines, bitints, false);
+    uint64_t co2 =
+        single_out(inp, filtered, buffer, bits, lines, bitints, true);
+
+    printf("%ld * %ld = %ld\n", ox, co2, ox * co2);
+
+    bitfield_free(buffer);
+    bitfield_free(filtered);
+    free(inp);
+}
