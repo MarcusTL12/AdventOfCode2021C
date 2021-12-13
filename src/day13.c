cfbc20daabaa4fb5677bb191a0bf88142613a54f
MarcusTL12 <marcuspaafjellet@gmail.com>
2021-12-13 18:17:43 +0100

    Day 13

--- src/day13.c
+++ src/day13.c
@@ -2,6 +2,7 @@
 #include <stdbool.h>
 #include <stdio.h>
 
+#include "../AoC_C_utils/src/bitfield.h"
 #include "../AoC_C_utils/src/file_util.h"
 #include "../AoC_C_utils/src/show.h"
 #include "../AoC_C_utils/src/vector.h"
@@ -31,7 +32,8 @@ static void parse_input(char *filename, vec_t *coords, vec_t *folds) {
         c += 2;
         cnn[1] = atoll(c);
         while (isdigit(*c)) c++;
-        while (*c && !isdigit(*c)) c++;
+        while (*c && !isalpha(*c)) c++;
+        vec_push(folds, &cnn);
     }
 
     free(inp);
@@ -39,7 +41,7 @@ static void parse_input(char *filename, vec_t *coords, vec_t *folds) {
 
 void d13p1() {
     vec_t coords, folds;
-    parse_input("input/day13/ex1", &coords, &folds);
+    parse_input("input/day13/input", &coords, &folds);
 
     size_t *cnn = vec_get(&folds, 0);
     for (size_t i = 0; i < coords.len; i++) {
@@ -49,10 +51,71 @@ void d13p1() {
         }
     }
 
-    // TODO: make bitfield
+    size_t maxx = 0, maxy = 0;
+
+    for (size_t i = 0; i < coords.len; i++) {
+        size_t *coord = vec_get(&coords, i);
+        maxx = coord[0] > maxx ? coord[0] : maxx;
+        maxy = coord[0] > maxy ? coord[0] : maxy;
+    }
+
+    size_t width = maxx + 1, height = maxy + 1;
+
+    size_t bitints = bitfield_ints(width * height);
+    uint64_t *grid = bitfield_create(width * height);
+
+    for (size_t i = 0; i < coords.len; i++) {
+        size_t *coord = vec_get(&coords, i);
+        bitfield_set(grid, coord[0] + coord[1] * width, true);
+    }
+
+    size_t ans = bitfield_count(grid, bitints);
+
+    printf("%zu\n", ans);
 
     vec_free(&coords);
     vec_free(&folds);
 }
 
-void d13p2() {}
+void d13p2() {
+    vec_t coords, folds;
+    parse_input("input/day13/input", &coords, &folds);
+
+    for (size_t i = 0; i < folds.len; i++) {
+        size_t *cnn = vec_get(&folds, i);
+        for (size_t j = 0; j < coords.len; j++) {
+            size_t *coord = vec_get(&coords, j);
+            if (coord[cnn[0]] > cnn[1]) {
+                coord[cnn[0]] = 2 * cnn[1] - coord[cnn[0]];
+            }
+        }
+    }
+
+    size_t maxx = 0, maxy = 0;
+
+    for (size_t i = 0; i < coords.len; i++) {
+        size_t *coord = vec_get(&coords, i);
+        maxx = coord[0] > maxx ? coord[0] : maxx;
+        maxy = coord[1] > maxy ? coord[1] : maxy;
+    }
+
+    size_t width = maxx + 1, height = maxy + 1;
+
+    size_t bitints = bitfield_ints(width * height);
+    uint64_t *grid = bitfield_create(width * height);
+
+    for (size_t i = 0; i < coords.len; i++) {
+        size_t *coord = vec_get(&coords, i);
+        bitfield_set(grid, coord[0] + coord[1] * width, true);
+    }
+
+    for (size_t y = 0; y < height; y++) {
+        for (size_t x = 0; x < width; x++) {
+            printf("%s", bitfield_get(grid, x + y * width) ? "██" : "  ");
+        }
+        printf("\n");
+    }
+
+    vec_free(&coords);
+    vec_free(&folds);
+}
