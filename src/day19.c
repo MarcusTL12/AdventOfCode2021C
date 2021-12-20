e2d804c0b35a1f13b1036d2f3c38477160f5d4a6
MarcusTL12 <marcuspaafjellet@gmail.com>
2021-12-20 10:37:10 +0100

    Started day 19

---
+++ src/day19.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#include "../AoC_C_utils/src/file_util.h"
+#include "../AoC_C_utils/src/vector.h"
+
+static vec_t parse_input(char *filename) {
+    // vec_t v = vec_create(sizeof)
+}
+
+void d19p1() {
+    printf("Hei!\n");
+}
+
+void d19p2() {}
--- src/days.h
+++ src/days.h
@@ -35,11 +35,13 @@ void d17p1();
 void d17p2();
 void d18p1();
 void d18p2();
+void d19p1();
+void d19p2();
 
-#define AMT_DAYS 18
+#define AMT_DAYS 19
 
 void (*days[])() = {d1p1,  d1p2,  d2p1,  d2p2,  d3p1,  d3p2,  d4p1,  d4p2,
                     d5p1,  d5p2,  d6p1,  d6p2,  d7p1,  d7p2,  d8p1,  d8p2,
                     d9p1,  d9p2,  d10p1, d10p2, d11p1, d11p2, d12p1, d12p2,
                     d13p1, d13p2, d14p1, d14p2, d15p1, d15p2, d16p1, d16p2,
-                    d17p1, d17p2, d18p1, d18p2};
+                    d17p1, d17p2, d18p1, d18p2, d19p1, d19p2};
